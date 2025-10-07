e677f5f5ac3d171d8deec71872a5b551581e3bd3
Kaua-Vidal <kauakauavidalpc@gmail.com>
2025-10-07 17:57:13 -0300

    Listando os usuarios via GET

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ const app = express()
 app.use(express.json())
 const users = []
 
-                // Pedido, Resposta
-app.get('/usuarios', (req, res) => {
+                     // Pedido, Resposta
+app.get('/usuarios', async (req, res) => {
+
+    const users = await prisma.user.findMany()
     
     res.status(200).json(users)
 })
@@ -16,7 +18,7 @@ app.get('/usuarios', (req, res) => {
 app.post('/usuarios', async (req, res) => {
 
     //usando await é uma promisse
-    await prisma.user.create ({
+    const user = await prisma.user.create ({
         data: {
             email: req.body.email,
             age: req.body.age,
@@ -24,7 +26,7 @@ app.post('/usuarios', async (req, res) => {
         }
     })
 
-    res.status(201).json({message: "Usuário criado com sucesso"})
+    res.status(201).json(user)
 })
 
 app.listen(3000)
