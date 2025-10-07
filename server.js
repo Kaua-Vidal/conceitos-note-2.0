be82a375370509e69cbbd283f34e762240d3386d
Kaua-Vidal <kauakauavidalpc@gmail.com>
2025-10-07 17:52:18 -0300

    Enviando um novo usuario via POST

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 import express from 'express'
+import { PrismaClient } from './generated/prisma/index.js'
+
+const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
@@ -10,8 +13,16 @@ app.get('/usuarios', (req, res) => {
     res.status(200).json(users)
 })
 
-app.post('/usuarios', (req, res) => {
-    users.push(req.body)
+app.post('/usuarios', async (req, res) => {
+
+    //usando await é uma promisse
+    await prisma.user.create ({
+        data: {
+            email: req.body.email,
+            age: req.body.age,
+            name: req.body.name
+        }
+    })
 
     res.status(201).json({message: "Usuário criado com sucesso"})
 })
